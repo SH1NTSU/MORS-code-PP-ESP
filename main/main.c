b3c2f6c5d7177cec9ae72f2115898bd88ac67cb7
Marcel Budziszewski <u84_marbud_waw@technischools.com>
2024-06-06 00:39:42 +0200

    hey ho it works

--- main/main.c
+++ main/main.c
@@ -8,12 +8,15 @@
 #include <esp_event.h>
 #include <esp_netif.h>
 #include <string.h>
+#include <http_rest_client.h>
+#include <cJSON.h>
 
 #define BLINK_PERIOD (1000)
+#define BLANK_PERIOD (100)
 
 static const char *TAG = "MAIN";
 
-esp_err_t message_handler(httpd_req_t *req)
+esp_err_t convert_handler(httpd_req_t *req)
 {
     char *request_content = (char *)calloc(req->content_len + 1, sizeof(char));
     if (request_content == NULL)
@@ -26,19 +29,19 @@ esp_err_t message_handler(httpd_req_t *req)
     size_t length = strlen(request_content);
     for (size_t i = 0; i < length; i++)
     {
-        if (request_content[i] == '_')
+        if (request_content[i] == '-')
         {
             gpio_set_level(GPIO_NUM_2, 1);
             vTaskDelay(pdMS_TO_TICKS(BLINK_PERIOD * 3));
             gpio_set_level(GPIO_NUM_2, 0);
-            vTaskDelay(pdMS_TO_TICKS(BLINK_PERIOD));
+            vTaskDelay(pdMS_TO_TICKS(BLANK_PERIOD));
         }
         else if (request_content[i] == '.')
         {
             gpio_set_level(GPIO_NUM_2, 1);
             vTaskDelay(pdMS_TO_TICKS(BLINK_PERIOD));
             gpio_set_level(GPIO_NUM_2, 0);
-            vTaskDelay(pdMS_TO_TICKS(BLINK_PERIOD));
+            vTaskDelay(pdMS_TO_TICKS(BLANK_PERIOD));
         }
         else if (request_content[i] == ' ')
         {
@@ -51,8 +54,142 @@ esp_err_t message_handler(httpd_req_t *req)
     return ESP_OK;
 }
 
+esp_err_t message_handler(httpd_req_t *req)
+{
+    ESP_LOGI(TAG, "Received POST request for /message");
+
+    char *request_content = (char *)calloc(req->content_len + 1, sizeof(char));
+    if (request_content == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to allocate memory for request content");
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to allocate memory");
+        return ESP_ERR_NO_MEM;
+    }
+
+    ssize_t ret = httpd_req_recv(req, request_content, req->content_len);
+    if (ret <= 0)
+    {
+
+        ESP_LOGE(TAG, "Failed to receive request content");
+        free(request_content);
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to receive request content");
+        return ESP_FAIL;
+    }
+
+    request_content[ret] = '\0';
+
+    httpd_req_recv(req, request_content, req->content_len);
+
+    const char *PUSHOVER_ROOT_CA = "-----BEGIN CERTIFICATE-----\n"
+                                   "MIIDjjCCAnagAwIBAgIQAzrx5qcRqaC7KGSxHQn65TANBgkqhkiG9w0BAQsFADBh\n"
+                                   "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
+                                   "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH\n"
+                                   "MjAeFw0xMzA4MDExMjAwMDBaFw0zODAxMTUxMjAwMDBaMGExCzAJBgNVBAYTAlVT\n"
+                                   "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n"
+                                   "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IEcyMIIBIjANBgkqhkiG\n"
+                                   "9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuzfNNNx7a8myaJCtSnX/RrohCgiN9RlUyfuI\n"
+                                   "2/Ou8jqJkTx65qsGGmvPrC3oXgkkRLpimn7Wo6h+4FR1IAWsULecYxpsMNzaHxmx\n"
+                                   "1x7e/dfgy5SDN67sH0NO3Xss0r0upS/kqbitOtSZpLYl6ZtrAGCSYP9PIUkY92eQ\n"
+                                   "q2EGnI/yuum06ZIya7XzV+hdG82MHauVBJVJ8zUtluNJbd134/tJS7SsVQepj5Wz\n"
+                                   "tCO7TG1F8PapspUwtP1MVYwnSlcUfIKdzXOS0xZKBgyMUNGPHgm+F6HmIcr9g+UQ\n"
+                                   "vIOlCsRnKPZzFBQ9RnbDhxSJITRNrw9FDKZJobq7nMWxM4MphQIDAQABo0IwQDAP\n"
+                                   "BgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAdBgNVHQ4EFgQUTiJUIBiV\n"
+                                   "5uNu5g/6+rkS7QYXjzkwDQYJKoZIhvcNAQELBQADggEBAGBnKJRvDkhj6zHd6mcY\n"
+                                   "1Yl9PMWLSn/pvtsrF9+wX3N3KjITOYFnQoQj8kVnNeyIv/iPsGEMNKSuIEyExtv4\n"
+                                   "NeF22d+mQrvHRAiGfzZ0JFrabA0UWTW98kndth/Jsw1HKj2ZL7tcu7XUIOGZX1NG\n"
+                                   "Fdtom/DzMNU+MeKNhJ7jitralj41E6Vf8PlwUHBHQRFXGU7Aj64GxJUTFy8bJZ91\n"
+                                   "8rGOmaFvE7FBcf6IKshPECBV1/MUReXgRPTqh5Uykw7+U0b6LJ3/iyK5S9kJRaTe\n"
+                                   "pLiaWN0bfVKfjllDiIGknibVb63dDcY3fe0Dkhvld1927jyNxF1WW6LZZm6zNTfl\n"
+                                   "MrY=\n"
+                                   "-----END CERTIFICATE-----\n";
+
+    const char *userToken = "ue9gui8abwvpgtu71kico75kkqs7r4";
+    const char *apiToken = "ahx1fn2vhkea1z1zr6bhwo7m6b99r4";
+    const char *pushoverApiEndpoint = "https://api.pushover.net/1/messages.json";
+
+    esp_err_t wifi_init_err = esp_wifi_start();
+    if (wifi_init_err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to initialize Wi-Fi: %d", wifi_init_err);
+        free(request_content);
+        return ESP_FAIL;
+    }
+
+    cJSON *notification = cJSON_CreateObject();
+    cJSON_AddStringToObject(notification, "token", apiToken);
+    cJSON_AddStringToObject(notification, "user", userToken);
+    cJSON_AddStringToObject(notification, "message", request_content);
+    cJSON_AddStringToObject(notification, "title", "ESP32 Notification");
+    cJSON_AddStringToObject(notification, "device", "esp32");
+
+    char *jsonStringNotification = cJSON_PrintUnformatted(notification);
+    ESP_LOGE(TAG, "JSON: %s", jsonStringNotification);
+    cJSON_Delete(notification);
+
+    esp_http_client_config_t config = {
+        .url = pushoverApiEndpoint,
+        .cert_pem = PUSHOVER_ROOT_CA,
+        .method = HTTP_METHOD_POST,
+        .buffer_size = strlen(jsonStringNotification),
+        .query = jsonStringNotification};
+
+    if (config.cert_pem == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to load root CA certificate");
+        free(jsonStringNotification);
+        free(request_content);
+        return ESP_FAIL;
+    }
+
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    if (client == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to initialize HTTP client");
+        free(jsonStringNotification);
+        free(request_content);
+        return ESP_FAIL;
+    }
+    ESP_LOGE(TAG, "client initialized");
+
+    esp_http_client_set_header(client, "Content-Type", "application/x-www-form-urlencoded");
+
+    char *payload = NULL;
+    asprintf(&payload, "token=%s&user=%s&device=%s&title=%s&message=%s",
+             apiToken, userToken, "esp32", "ESP32 Notification", request_content);
+
+    esp_http_client_set_post_field(client, payload, strlen(payload));
+
+    // Send the HTTP request
+    esp_err_t http_err = esp_http_client_perform(client);
+    if (http_err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "HTTP POST request failed: %d", http_err);
+    }
+    else
+    {
+        ESP_LOGI(TAG, "HTTP POST request success");
+    }
+
+    int status_code = esp_http_client_get_status_code(client);
+    ESP_LOGI(TAG, "HTTP Status = %d", status_code);
+
+    httpd_resp_send(req, NULL, 0);
+
+    free(request_content);
+    free(jsonStringNotification);
+    free(payload);
+
+    return ESP_OK;
+}
+
 httpd_uri_t handler_1 = {
     .uri = "/convert",
+    .method = HTTP_POST,
+    .handler = convert_handler,
+    .user_ctx = NULL};
+
+httpd_uri_t handler_2 = {
+    .uri = "/message",
     .method = HTTP_POST,
     .handler = message_handler,
     .user_ctx = NULL};
@@ -65,6 +202,7 @@ static void got_ip_event_handler(void *arg, esp_event_base_t event_base, int32_t
 
 void app_main(void)
 {
+
     ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT));
 
     esp_log_level_set(TAG, ESP_LOG_DEBUG);
@@ -92,8 +230,8 @@ void app_main(void)
 
     wifi_config_t wifi_cfg = {
         .sta = {
-            .ssid = "........",
-            .password = "......."}};
+            .ssid = "UPC7912814",
+            .password = "Jv8zspbckxHs"}};
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_cfg));
     ESP_ERROR_CHECK(esp_wifi_start());
@@ -109,6 +247,7 @@ void app_main(void)
     {
         ESP_LOGI(TAG, "HTTP server started");
         ESP_ERROR_CHECK(httpd_register_uri_handler(httpd_handle, &handler_1));
+        ESP_ERROR_CHECK(httpd_register_uri_handler(httpd_handle, &handler_2));
     }
     else
     {
